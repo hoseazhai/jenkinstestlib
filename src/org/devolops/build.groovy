40787981087f6d1d1ae9bda5ce5012e2ff41748f
saltstack <zhaihouxiang@gmail.com>
2020-04-06 13:43:37 +0800

    Update build.groovy

--- src/org/devolops/build.groovy
+++ src/org/devolops/build.groovy
@@ -1,11 +1,13 @@
 package org.devolops
 
 //构建类型
-def Build(buildTYpe, buildShell){
-    def buildTools = ["maven":"maven,"gradle":"gradle","npm":"npm]
+def Build(buildType, buildShell){
+    def buildTools = ["mvn":"maven","gradle":"gradle","npm":"npm]
     buildHome = tool buildTools[buildType]
     
     println("当前选择的构建类型为 ${buildType}")
+    
+    sh "${buildHome}/bin/${buildtype} ${buildShell}"
     
 }
     
